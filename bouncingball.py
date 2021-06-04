d71e17f8131ac6a56a7f19e4a587955f319a605e
Aditya <aranganath@ucmerced.edu>
2021-06-04 00:01:31 -0700

    Custom commit

--- bouncingball.py
+++ bouncingball.py
@@ -3,28 +3,52 @@ import numpy as np
 from cv2 import VideoWriter, VideoWriter_fourcc
 from pdb import set_trace
 import matplotlib.pyplot as plt
+import torch
+from torch import nn
 
-width = 1280
-height = 720
-FPS = 24
-seconds = 10
-radius = 150
-paint_h = int(height/2)
 
-fourcc = VideoWriter_fourcc(*'MP42')
-video = VideoWriter('./data/circle_noise.avi', fourcc, float(FPS), (width, height))
+class RNNgenerator(nn.Module):
+	"""docstring for RNNgenerator"""
+	def __init__(self, height, FPS, seconds, radius):
+		super(RNNgenerator, self).__init__()
+		self.arg = arg
+		self.VideoData = []
+		self.width = 1280
+		self.height = 720
+		self.FPS = 24
+		self.seconds = 10
+		self.radius = 150
+		self.paint_h = int(height/2)
 
-for paint_x in range(-radius, width+radius, 6):
-    frame = 255*np.ones( 
-                   (height, width, 3), 
-                              dtype=np.uint8)
-    cv2.circle(frame, (paint_x, paint_h), radius, (0, 0, 0), -1)
-    video.write(frame)
 
-video.release()
+	def GenerateData(self):
+		
 
-Vc = cv2.VideoCapture('./data/circle_noise.avi')
-ret, frame = Vc.read()
+		fourcc = VideoWriter_fourcc(*'MP42')
+		video = VideoWriter('./data/circle_noise.avi', fourcc, float(FPS), (width, height))
+
+		if not self.VideoData:
+			for paint_x in range(-radius, width+radius, 6):
+			    frame = 255*np.ones( 
+			                   (height, width, 3), 
+			                              dtype=np.uint8)
+			    
+			    cv2.circle(frame, (paint_x, paint_h), radius, (0, 0, 0), -1)
+			    
+			    # Store the frames in the list
+
+			    self.VideoData.append(frame)
+
+			    video.write(frame)
+
+			video.release()
+			self.VideoData = np.stack(self.VideoData, axis=0)
+
+			Vc = cv2.VideoCapture('./data/circle_noise.avi')
+			ret, frame = Vc.read()
+		else:
+			print("Data is already generated")
+
 
 
 while ret:
